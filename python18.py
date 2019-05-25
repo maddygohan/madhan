48e5bcb04506b727b05b667eb38160bec3774fd8
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-25 17:53:01 +0530

    Update python18.py

--- python18.py
+++ python18.py
@@ -1,12 +1,11 @@
 n=int(input(""))
 m=int(input(""))
 for num in range(n,m+1):
-    order=len(str(num))
     sum=0
     temp=num
     while(temp>0):
       dig=temp%10
-      sum+=dig**order
+      sum+=dig**3
       temp//=10
     if(num==sum):
      print(num)
