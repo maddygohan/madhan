834040b526cbb0401ef08ca779cf0f301d2c4493
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-25 18:09:32 +0530

    Create python19.py

---
+++ python19.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if(n==1):
+        return n
+    else:
+        return n *  factorial(n-1)
+n=int(input(""))
+if(n==0):
+    print("factorial doesn't having negative number")
+elif(n==0):
+    print("1")
+else:
+    print("",factorial(n))
