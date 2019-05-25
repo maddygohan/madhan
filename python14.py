291fc7f39c50689cdcd5b5710afc8ce77a09dcea
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-25 15:34:43 +0530

    Create python14.py

---
+++ python14.py
@@ -0,0 +1,8 @@
+n=int(input(""))
+k=int(input(""))
+if(n>100000 and k>100000):
+    print("enter values in between 100000")
+else:
+    for i in range(n,k):
+        if(i%2!=0):
+            print(i)
