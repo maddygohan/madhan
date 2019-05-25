4e3f503e5e14682efe70ab442831db12adb4366b
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-25 16:27:27 +0530

    Create python16.py

---
+++ python16.py
@@ -0,0 +1,9 @@
+n=int(input(""))
+k=int(input(""))
+for num in range(n+1,k):
+    if num>1:
+        for i in range(2,num):
+            if(num%i)==0:
+                break
+        else:
+             print(num)
