11f8ca30df6801b81ede13febbd5441bfa341d0c
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-29 13:07:10 +0530

    Update python9.py

--- python9.py
+++ python9.py
@@ -1,9 +1,9 @@
-a,b=input("").split()
-a=int(a)
-b=int(b)
+a,b=map(int,input().split())
+li=[]
+li= list(map(int, input("").split())) 
 s=0
-for i in range(1,a+1):
-    print(i,end=' ')
-for j in range(0,b+1):
-    s=s+j
-print('\n',s)
+for j in range(b):
+    s=s+li[j]
+print(s)
+
+
