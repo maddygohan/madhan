b8cd92daf79e73c07c02952b05c8fb6cb59da18d
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-05-29 12:34:32 +0530

    Update python9.py

--- python9.py
+++ python9.py
@@ -1,8 +1,9 @@
-a=int(input(""))
-b=int(input(""))
+a,b=input("").split()
+a=int(a)
+b=int(b)
 s=0
 for i in range(1,a+1):
     print(i,end=' ')
 for j in range(0,b+1):
     s=s+j
-print("",s)
+print('\n',s)
