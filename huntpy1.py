31125517376369c995bf9f283f63a9539338f104
maddygohan <50616571+maddygohan@users.noreply.github.com>
2019-07-12 12:19:43 +0530

    Create huntpy1.py

---
+++ huntpy1.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=[int(x) for x in input().split()]
+arr2=[]
+for i in arr:
+    for j in range(arr.index(i)+1,len(arr)):
+        if(i==arr[j]):
+            if i not in arr2:
+                arr2.append(i)
+                break
+arr2.sort()
+{print(x,end=" ") for x in arr2}
